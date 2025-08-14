62f74ae2b6339dc5716fed88ce1d6fe074d9c539
Utsav Vachhani (uv) <uvachhani03@gmail.com>
2025-08-14 17:42:41 +0000

    6th commit

--- client/src/constants/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const AUTH = 'AUTH';
-export const LOGOUT = 'LOGOUT';
-export const UPDATE_PROFILE ="UPDATE_PROFILE";
-export const REGISTER = "REGISTER";
-export const EMAILS = "EMAILS";
--- client/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux'
-import authReducer from './auth.js'
-
-export default combineReducers({
-    auth: authReducer,
-})
