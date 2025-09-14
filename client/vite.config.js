0a41204cc35abd040953055637910242b7ecbadb
Utsav Vachhani (uv) <uvachhani03@gmail.com>
2025-09-14 05:30:51 +0000

    16th commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  build: {
-    chunkSizeWarningLimit: 15000 
-  }
+  // build: {
+  //   chunkSizeWarningLimit: 15000 
+  // }
 })
